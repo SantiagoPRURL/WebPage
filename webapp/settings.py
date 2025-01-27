387334640ffbabd8ff660733bc9a9aa0c56ef527
SantiagoPRURL <santiagopr123@gmail.com>
2025-01-27 03:17:44 -0500

    test-new

--- webapp/settings.py
+++ webapp/settings.py
@@ -130,12 +130,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# En producción, los archivos estáticos se recopilarán aquí
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Usamos WhiteNoise para servir los archivos estáticos
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
+    # Si tienes archivos estáticos personalizados o externos en un directorio, puedes agregarlo así
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ] 
     
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'webapp/static')]
